a5f2309d2e49d37e99a770c1c9d948464ee42795
Yulia2003 <sergeeva684p@gmail.com>
2024-10-04 21:14:17 +0300

    first steps app-react

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+﻿// src/components/Button.jsx
+import React from 'react';
+import './Button.css'; // Подключение стилей для кнопки
+
+function Button({ label, onClick, color = 'blue', size = 'medium' }) {
+  return (
+    <button
+      className={`btn btn-${color} btn-${size}`} // Использование динамических классов
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+}
+
+export default Button;
---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+﻿// src/pages/Home.jsx
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Travel Planner</h1>
+      <p>Discover new destinations and plan your trips easily!</p>
+    </div>
+  );
+}
+
+export default Home;
